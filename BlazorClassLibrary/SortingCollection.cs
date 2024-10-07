203abbcabc9b36600cb3f929edd4b28f4f9dbdb9
RusandeKodare <o.p.j@hotmail.com>
2024-10-07 16:06:24 +0200

    +Created class for sorting and added two methods +Made changes in @code areea of TableLab page

---
+++ BlazorClassLibrary/SortingCollection.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorClassLibrary
+{
+	public class SortingCollection
+	{
+
+		public List<User> SortByName(List<User> users)
+		{
+			List<User> usersAscending = users.OrderBy(movie => movie.Name).ToList();
+			if (users.SequenceEqual(usersAscending))
+			{
+				users = users.OrderByDescending(movie => movie.Name).ToList();
+				return users;
+			}
+				users = users.OrderBy(movie => movie.Name).ToList();
+				return users;
+		}
+
+		public List<User> SortByID(List<User> users)
+		{
+			List<User> usersAscending = users.OrderBy(movie => movie.Id).ToList();
+
+			if (users.SequenceEqual(usersAscending))
+			{
+				users = users.OrderByDescending(movie => movie.Id).ToList();
+				return users;
+			}
+			users = users.OrderBy(movie => movie.Id).ToList();
+			return users;
+		}
+	}
+}
