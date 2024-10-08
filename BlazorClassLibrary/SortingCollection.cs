a81e953039cc1643595cb02e9849a9fcb0e85d03
RusandeKodare <o.p.j@hotmail.com>
2024-10-08 10:18:24 +0200

    +Finished sorting methods in SortingCollection class +Commented out GetUsersFromApi component +Created new component SearchInputAndMessage +Removed unecessary code and added components to TableLab Component +Created css file for TableLab +Created UserInfoForSpecificUser component and used it in TableLab

--- BlazorClassLibrary/SortingCollection.cs
+++ BlazorClassLibrary/SortingCollection.cs
@@ -8,30 +8,76 @@ namespace BlazorClassLibrary
 {
 	public class SortingCollection
 	{
-
-		public List<User> SortByName(List<User> users)
-		{
-			List<User> usersAscending = users.OrderBy(movie => movie.Name).ToList();
-			if (users.SequenceEqual(usersAscending))
-			{
-				users = users.OrderByDescending(movie => movie.Name).ToList();
-				return users;
-			}
-				users = users.OrderBy(movie => movie.Name).ToList();
-				return users;
-		}
-
 		public List<User> SortByID(List<User> users)
 		{
-			List<User> usersAscending = users.OrderBy(movie => movie.Id).ToList();
+			List<User> usersAscending = users.OrderBy(user => user.Id).ToList();
 
 			if (users.SequenceEqual(usersAscending))
 			{
-				users = users.OrderByDescending(movie => movie.Id).ToList();
+				users = users.OrderByDescending(user => user.Id).ToList();
 				return users;
 			}
 			users = users.OrderBy(movie => movie.Id).ToList();
 			return users;
 		}
+		public List<User> SortByName(List<User> users)
+		{
+			List<User> usersAscending = users.OrderBy(user => user.Name).ToList();
+			if (users.SequenceEqual(usersAscending))
+			{
+				users = users.OrderByDescending(user => user.Name).ToList();
+				return users;
+			}
+			users = users.OrderBy(user => user.Name).ToList();
+			return users;
+		}
+
+		public List<User> SortByUserName(List<User> users)
+		{
+			List<User> usersAscending = users.OrderBy(user => user.UserName).ToList();
+			if (users.SequenceEqual(usersAscending))
+			{
+				users = users.OrderByDescending(user => user.UserName).ToList();
+				return users;
+			}
+			users = users.OrderBy(user => user.UserName).ToList();
+			return users;
+		}
+		public List<User> SortByEmail(List<User> users)
+		{
+			List<User> usersAscending = users.OrderBy(user => user.Email).ToList();
+			if (users.SequenceEqual(usersAscending))
+			{
+				users = users.OrderByDescending(user => user.Email).ToList();
+				return users;
+			}
+			users = users.OrderBy(user => user.Email).ToList();
+			return users;
+		}
+		public List<User> SortByCity(List<User> users)
+		{
+			List<User> usersAscending = users.OrderBy(user => user.Address.City).ToList();
+			if (users.SequenceEqual(usersAscending))
+			{
+				users = users.OrderByDescending(user => user.Address.City).ToList();
+				return users;
+			}
+			users = users.OrderBy(user => user.Address.City).ToList();
+			return users;
+		}
+		public List<User> SortByCompanyName(List<User> users)
+		{
+			List<User> usersAscending = users.OrderBy(user => user.Company.Name).ToList();
+			if (users.SequenceEqual(usersAscending))
+			{
+				users = users.OrderByDescending(user => user.Company.Name).ToList();
+				return users;
+			}
+			users = users.OrderBy(user => user.Company.Name).ToList();
+			return users;
+		}
+
+
+
 	}
 }
